7774bb2467be0a69323c7091ebbec33bdbad53d4
Krzysztof Czarnecki <czarnecki.krzysiek@gmail.com>
2022-05-22 15:15:16 +0200

    plot armor

---
+++ plot.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+
+function form_data(){
+    cat $1 | jq "[[.${2}[][0]], [.${2}[][4]]] | transpose | .[][]" | paste - - > ${3}
+    echo "output data: ${3}"
+}
+
+function make_plot(){
+    now=`date "+%d-%m-%Y"`
+    gnuplot -persist <<-EOFMarker
+
+    set terminal png size 800,600
+    set output "${1}.png"
+    set xlabel "offset in days"
+    set ylabel "number"
+    set grid
+    set title "${1} by UA (${now})" font ",14" textcolor rgbcolor "royalblue"
+    plot "${1}.txt" using 1:2 with linespoints title "${1}"
+EOFMarker
+    echo "output plot: ${1}.png"
+}
+
+
+form_data rus-loss-by-ua.json armor armor.txt
+make_plot armor
